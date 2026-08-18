fb2badaa6b9bf540829621152eec79f1ed7ad5c9
franconferreira <franconferreira@gmail.com>
2026-08-17 21:20:22 -0300

    feat: Implementacion de Calificacion y Comentarios en memoria

--- marketplace/src/main/java/com/uade/EcommerceUniformes/marketplace/controllers/CalificacionController.java
+++ marketplace/src/main/java/com/uade/EcommerceUniformes/marketplace/controllers/CalificacionController.java
@@ -1,4 +1,31 @@
 package com.uade.EcommerceUniformes.marketplace.controllers;
 
+import com.uade.EcommerceUniformes.marketplace.entity.Calificacion;
+import com.uade.EcommerceUniformes.marketplace.entity.Comentario;
+import com.uade.EcommerceUniformes.marketplace.service.CalificacionSerivice;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("calificaciones")
 public class CalificacionController {
+
+    @GetMapping
+    public ArrayList<Calificacion> getCalificaciones() {
+        CalificacionSerivice calificacionService = new CalificacionSerivice();
+        return calificacionService.getCalificaciones();
+    }
+
+    @GetMapping("/producto/{productoId}")
+    public Calificacion getCalificacionByProductoId(@PathVariable int productoId) {
+        CalificacionSerivice calificacionService = new CalificacionSerivice();
+        return calificacionService.getCalificacionByProductoId(productoId);
+    }
+
+    @PostMapping("/producto/{productoId}")
+    public Calificacion addComentario(@PathVariable int productoId, @RequestBody Comentario comentario) {
+        CalificacionSerivice calificacionService = new CalificacionSerivice();
+        return calificacionService.addComentario(productoId, comentario);
+    }
 }
--- marketplace/src/main/java/com/uade/EcommerceUniformes/marketplace/repository/CalificacionRepository.java
+++ marketplace/src/main/java/com/uade/EcommerceUniformes/marketplace/repository/CalificacionRepository.java
@@ -1,4 +1,47 @@
 package com.uade.EcommerceUniformes.marketplace.repository;
 
+import com.uade.EcommerceUniformes.marketplace.entity.Calificacion;
+import com.uade.EcommerceUniformes.marketplace.entity.Comentario;
+import com.uade.EcommerceUniformes.marketplace.entity.Producto;
+
+import java.util.ArrayList;
+
 public class CalificacionRepository {
+    public ArrayList<Calificacion> calificaciones = new ArrayList<>();
+
+    public ArrayList<Calificacion> getCalificaciones() {
+        return this.calificaciones;
+    }
+
+    public Calificacion getCalificacionByProductoId(int productoId) {
+        for (Calificacion c : calificaciones) {
+            if (c.getProducto() != null && c.getProducto().getId() == productoId) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public Calificacion addComentario(Producto producto, Comentario comentario) {
+        Calificacion calificacion = getCalificacionByProductoId(producto.getId());
+        if (calificacion == null) {
+            calificacion = Calificacion.builder()
+                    .producto(producto)
+                    .calificacionTotal(0)
+                    .comentarios(new ArrayList<>())
+                    .build();
+            calificaciones.add(calificacion);
+        }
+
+        calificacion.getComentarios().add(comentario);
+
+        // Recalcular promedio de estrellas
+        int suma = 0;
+        for (Comentario com : calificacion.getComentarios()) {
+            suma += com.getCalificacion();
+        }
+        calificacion.setCalificacionTotal(suma / calificacion.getComentarios().size());
+
+        return calificacion;
+    }
 }
